6212d49425bbeb50dc407e062952e932e2b8f6c5
Jamin George <jaminchristos@gmail.com>
2024-08-25 17:38:33 -0700

    Added more comments

--- PriorityExpiryCache/main/main.go
+++ PriorityExpiryCache/main/main.go
@@ -8,7 +8,12 @@ import (
 	"time"
 )
 
-/* Time and Space complexity
+/* To implement this I have used the following datastructures.
+1. Map: to Get and Set the items. And manage the capacity
+2. Doubly Linked List: to get the least recently used item
+3. Two Min Heap - Priority Queues: To get the item with the least priority and the expired item.
+4. The model.go file has the structs and heap declaration.
+
 Time Complexity - O(logN) for most operations, including adding, editing, evicting, removing and updating items.
 Space Complexity - O(N) for storing up to N items in the cache.
 */
